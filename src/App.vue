696435b9835bc0a957e440da55b1920d3ada753f
gufranlazuardi <gufranlazuardi@gmail.com>
2024-12-25 17:32:24 +0700

    setup

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import { RouterView } from "vue-router";
+import Toaster from "@/components/ui/toast/Toaster.vue";
+import Layout from "./Layout.vue";
+</script>
+
+<template>
+  <Layout>
+    <RouterView />
+    <Toaster />
+  </Layout>
+</template>
