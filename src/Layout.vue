a08cf5ecb55ff4c8be0555d0931d1b327c253281
gufranlazuardi <gufranlazuardi@gmail.com>
2024-12-25 17:32:34 +0700

    feat: layout

---
+++ src/Layout.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
