0c51a9ef390ad808df98b7d09f9d3e7455f572b8
gufranlazuardi <gufranlazuardi@gmail.com>
2024-12-26 11:03:31 +0700

    fix: responsive

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,7 +3,9 @@ import LoginForm from "@/components/LoginForm.vue";
 </script>
 
 <template>
-  <div class="flex flex-col w-full items-center">
+  <div
+    class="flex flex-col w-full items-center max-w-screen-xl mx-auto px-[2.5rem] md:px-[4rem] lg:px-[4rem] xl:px-[4rem]"
+  >
     <LoginForm />
   </div>
 </template>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -3,7 +3,9 @@ import RegisterForm from "../components/RegisterForm.vue";
 </script>
 
 <template>
-  <div class="flex flex-col gap-3">
+  <div
+    class="flex flex-col w-full items-center max-w-screen-xl mx-auto px-[2.5rem] md:px-[4rem] lg:px-[4rem] xl:px-[4rem]"
+  >
     <RegisterForm />
   </div>
 </template>
